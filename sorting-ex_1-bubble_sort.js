c18d2a1a16be840d4e8428af8654b2cab9b01c5d
Juan Carlos Cancela <jccancela@aetion.com>
2019-09-17 14:04:00 -0300

    implemented sorting

--- sorting-ex_1-bubble_sort.js
+++ sorting-ex_1-bubble_sort.js
@@ -4,8 +4,26 @@
  * @author Juan Carlos Cancela <cancela.juancarlos@gmail.com>
  */
 // Complete the countSwaps function below.
-function countSwaps(a) {
+function countSwaps(arr) {
+    let swaps = 0;
+    const swap = j => {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        ++swaps;
+    };
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(j);
+            }
+        }
+    }
+    console.log(`Array is sorted in ${swaps} swaps.`);
+    console.log(`First Element: ${arr[0]}`);
+    console.log(`Last Element: ${arr[arr.length - 1]}`);
 }
 
 //Example
+const a = [6, 4, 1];
 console.log(countSwaps(a));
