545d64d3d28076cc5eeb42f1dc440959e24c63ec
Juan Carlos Cancela <jccancela@aetion.com>
2019-09-17 13:00:39 -0300

    bubble sort

---
+++ sorting-ex_1-bubble_sort.js
@@ -0,0 +1,11 @@
+/**
+ * @title Bubble Sort
+ * @statement check sorting-ex_1-bubble_sort.pdf
+ * @author Juan Carlos Cancela <cancela.juancarlos@gmail.com>
+ */
+// Complete the countSwaps function below.
+function countSwaps(a) {
+}
+
+//Example
+console.log(countSwaps(a));
